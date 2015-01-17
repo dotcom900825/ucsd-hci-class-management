a2c1b31f334a3cf00e2d0cdfdfe50d8a47849ffc
dotcom900825 <dotcom900825@gmail.com>
2015-01-16 16:07:46 -0800

    add studio name

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -13,4 +13,8 @@ class Studio < ActiveRecord::Base
     count = Submission.where(:assignment=>assignment).joins(:student).where(:users=>{studio_id: self.id}).count
     return count
   end
+
+  def name
+    studio_name
+  end
 end
