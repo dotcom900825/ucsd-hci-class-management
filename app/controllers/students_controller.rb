f1c7805d9c9c8f9626a3f78bc5eaff63ee83857c
Brian Soe <bsoe@ucsd.edu>
2016-01-05 22:00:44 -0800

    fixed bug on student count edited flash error message.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -23,14 +23,14 @@ class StudentsController < ApplicationController
   end
 
   def update
-    count = Studio.find(:studio_id).students_count
+    count = Studio.find(params[:student][:studio_id]).students_count
     if count >= 0 && count < STUDIO_CAP
       @student = Student.find(params[:id])
       @student.update_attribute(:studio_id, params[:student][:studio_id])
       flash[:success] = "Studio update success"
       redirect_to root_path
     else
-      flash[:error] = "The studio is full. Please try again"
+      flash[:error] = "The studio is full. Please try to enroll in another section"
       redirect_to :back # refresh the page
     end
   end
