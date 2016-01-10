5a768a7984af7144ca2f96b3e32861b18b6ef45c
Yu Xia <yu.xia@mycase.com>
2016-01-10 09:56:51 -0800

    add submit lab func in student lab page, fix bugs due to not login user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
   end
+
+  private
+  def not_authenticated
+    redirect_to login_path, alert: "Please login first"
+  end
 end
--- app/controllers/student_labs_controller.rb
+++ app/controllers/student_labs_controller.rb
@@ -1,5 +1,18 @@
 class StudentLabsController < ApplicationController
+  before_filter :require_login
+
   def index
     @student_labs = current_user.student_labs
+  end
+
+  def create
+    @student_lab = StudentLab.create(student_lab_params.merge(complete: true))
+    flash[:success] = "Submission success"
+    redirect_to student_labs_path
+  end
+
+  private
+  def student_lab_params
+    params.require(:student_lab).permit(:lab_id, :github_url, :complete)
   end
 end
